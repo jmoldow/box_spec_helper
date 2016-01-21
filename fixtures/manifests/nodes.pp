539ca15c5e8f081829bc629d6777f2caff308c82
Zack Smith <zsmith@box.com>
2016-01-20 22:23:50 -0800

    Switch to directory based environments

--- fixtures/manifests/nodes.pp
+++
@@ -1,3 +0,0 @@
-# This manifest should be empty. For rspec-puppet testing, we don't want to use node
-# definitions to load manifests automatically, since we target particular manifests at a
-# time for testing.
